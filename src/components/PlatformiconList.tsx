405b7c3187303eac339bfbe6abc4482ab1c67807
Timic Ivey <timicivey27@gmail.com>
2023-05-26 10:37:44 -0400

    Filter games by genre

--- src/components/PlatformiconList.tsx
+++ src/components/PlatformiconList.tsx
@@ -6,35 +6,34 @@ import {
 	FaLinux,
 	FaAndroid,
 } from "react-icons/fa";
-import {MdPhoneIphone} from "react-icons/md";
-import {SiNintendo} from "react-icons/si"
-import {BsGlobe} from "react-icons/bs"
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
 import { Platform } from "../hooks/useGames";
 import { HStack, Icon, Text } from "@chakra-ui/react";
 import { IconType } from "react-icons";
-
 
 interface Props {
 	platforms: Platform[];
 }
 
 const PlatformiconList = ({ platforms }: Props) => {
-    const iconMap: {[key: string]: IconType} = {
-        pc: FaWindows,
-        playstation: FaPlaystation,
-        xbox: FaXbox,
-        nintendo: SiNintendo,
-        mac: FaApple,
-        linux: FaLinux,
-        ios: MdPhoneIphone,
-        web: BsGlobe,
-        andriod: FaAndroid
-    }
+	const iconMap: { [key: string]: IconType } = {
+		pc: FaWindows,
+		playstation: FaPlaystation,
+		xbox: FaXbox,
+		nintendo: SiNintendo,
+		mac: FaApple,
+		linux: FaLinux,
+		ios: MdPhoneIphone,
+		web: BsGlobe,
+		andriod: FaAndroid,
+	};
 
 	return (
 		<HStack marginY={2}>
 			{platforms.map((platform) => (
-				<Icon as={iconMap[platform.slug]} color="gray.500"/>
+				<Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
 			))}
 		</HStack>
 	);
