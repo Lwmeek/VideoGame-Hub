d4824ba157d1fa7859382596b0ec0cdb4304dda3
Timic Ivey <timicivey27@gmail.com>
2024-05-24 11:07:46 -0400

    Making static data for genres

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: "3b97af380a0c409da0e90badd50ae7a4"
-    }
-})
+	baseURL: "https://api.rawg.io/api",
+	params: {
+		key: "012ba87bd2c5405498ae1bb895795842",
+	},
+});
