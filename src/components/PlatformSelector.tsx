2e90e07de0f2646d2e80189bbf612f54bce96776
Timic Ivey <timicivey27@gmail.com>
2024-05-29 10:23:21 -0400

    refactor and simplify gamequery

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,11 +4,13 @@ import usePlatforms, { Platforms } from "../hooks/usePlatforms";
 
 interface Props {
 	onSelectPlatform: (platform: Platforms) => void;
-	selectedPlatform: Platforms | null;
+	selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
 	const { data, error } = usePlatforms();
+
+	const selectedPlatform = data?.results.find(p => p.id === selectedPlatformId);
 
 	if (error) return null;
 
